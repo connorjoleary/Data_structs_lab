18756a899811c91906c877ace5259630871f7161
fippy24 <connor.trumpet@gmail.com>
2015-01-28 15:10:11 -0600

    makes a hashmap of the string

--- src/lab_1/main.java
+++ src/lab_1/main.java
@@ -1,13 +1,35 @@
 package lab_1;
-import java.util.Scanner;
+import java.util.*;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Type in your message: ");
-		String choice = keyboard.nextLine();
+		String text = keyboard.nextLine();
+		Map<Character,Integer> letters = get_frequency(text);
+		Iterator<Character> keySetIterator = letters.keySet().iterator();
+
+		while(keySetIterator.hasNext()){
+		  Character key = keySetIterator.next();
+		  System.out.println("key: " + key + " value: " + letters.get(key));
+		}
 		
+		
+	}
+	
+	public static Map<Character,Integer> get_frequency(String text){
+		Map<Character,Integer> letters = new Hashtable<Character,Integer>();
+		for (int i=0; i<text.length(); i++){
+			 if (letters.containsKey(text.charAt(i))){
+				 letters.put(text.charAt(i), letters.get(text.charAt(i))+1);
+			 }
+			 else {
+				 letters.put(text.charAt(i), 1);
+			 }
+		}
+		
+		return letters;
 	}
 
 }
