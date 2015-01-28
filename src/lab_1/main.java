a0e115ab5d881b1248d758c66fad3e627f94bf22
fippy24 <connor.trumpet@gmail.com>
2015-01-28 13:54:25 -0600

    more to main

--- src/lab_1/main.java
+++ src/lab_1/main.java
@@ -1,10 +1,13 @@
 package lab_1;
-
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Type in your message: ");
+		String choice = keyboard.nextLine();
+		
 	}
 
 }
