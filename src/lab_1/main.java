b78a37f4bf3dda0762acf027769701aa09a67491
fippy24 <connor.trumpet@gmail.com>
2015-02-05 12:29:44 -0700

    main2
    main 2 makes a string hashmap

--- src/lab_1/main.java
+++ src/lab_1/main.java
@@ -18,16 +18,36 @@ public class main {
 		}
 		int size = letters.size();
 		Tree theTree = new Tree();
-
+		int newKey;
 		for (int i = 0; i < size; i++) {
 			int minimal_key = get_minimum_key(letters, text.length());
-			
-
+			int frequency = letters.get(minimal_key);
 			letters.remove(minimal_key);
+			int minimal_key2 = get_minimum_key(letters, text.length());
+			int frequency2 = letters.get(minimal_key2);
+			letters.remove(minimal_key2);
+			newKey = combine_keys(minimal_key, minimal_key2);
+			System.out.println(newKey);
+			letters.put(newKey, frequency+frequency2);
 			//System.out.println(minimal_key + " " + (int) minimal_key);
 		}
-		theTree.displayTree();
+		//theTree.displayTree();
 
+	}
+	public static int combine_keys(int a, int b){
+		System.out.println("a= " +a+" b= "+b);
+		
+		if(a<100){
+			a*=10;
+		}
+		if(b<100){
+			b*=10;
+		}
+		//int lengthA = (int)(Math.log10(a)+1);
+		int lengthB = (int)(Math.log10(b)+1);
+		a= (int) (a*Math.pow(10, lengthB));
+		System.out.println(a);
+		return a+b;
 	}
 
 	public static Map<Integer, Integer> get_frequency(String text) {
