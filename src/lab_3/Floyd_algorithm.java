6bc2854b95a2fd8eb87204d52d39598486263c3d
fippy24 <connor.trumpet@gmail.com>
2015-04-05 01:26:24 -0600

    start of the lab

---
+++ src/lab_3/Floyd_algorithm.java
@@ -0,0 +1,18 @@
+package lab_3;
+
+public class Floyd_algorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int table[][]= new int[5][5];
+		
+		int table[][] = {{Integer.MAX_VALUE,50,Integer.MAX_VALUE,80,Integer.MAX_VALUE}
+		,{0,0,60,90,0}
+		,{0,0,0,0,40},
+		{0,0,20,0,70},
+		{0,50,0,0,0}};
+		
+		
+	}
+
+}
