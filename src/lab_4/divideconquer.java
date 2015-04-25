b87df9b29a8a8dd0c2479a7c535ac9bfb38bb95e
fippy24 <connor.trumpet@gmail.com>
2015-04-24 20:56:11 -0600

    new lab

---
+++ src/lab_4/divideconquer.java
@@ -0,0 +1,15 @@
+package lab_4;
+
+/*Implement the divide-and-conquer algorithm you learnt in the class to solve the closest pair of
+points problem. You could either hard-code or randomly generate points in your solution. The
+running time of your algorithm should be O(nlogn). Sample output of your solution should
+look like:*/
+
+public class divideconquer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/lab_ec/mergesort.java
@@ -0,0 +1,10 @@
+package lab_ec;
+
+public class mergesort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
