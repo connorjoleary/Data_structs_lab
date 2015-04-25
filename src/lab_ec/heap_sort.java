03f3f59d54d79fbd60cdb9844ac3d01b53fd9a38
fippy24 <connor.trumpet@gmail.com>
2015-04-25 00:07:51 -0600

    done except for none complete tree

--- src/lab_ec/heap_sort.java
+++ src/lab_ec/heap_sort.java
@@ -3,19 +3,19 @@ package lab_ec;
 import java.util.Random;
 import java.util.Arrays;
 public class heap_sort {
-	static int[] arr= new int[7];
+	static int[] arr= new int[4];
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		arr = make_initial_tree();
 		System.out.println(Arrays.toString(arr));
-		sort(0, 7);
+		sort(0, 4);
 		System.out.println(Arrays.toString(arr));
 	}
 	public static int[] make_initial_tree(){
-		int[] arr = new int[7];
-		for (int i=0; i<7; i++){
+		int[] arr = new int[4];
+		for (int i=0; i<4; i++){
 			Random generator = new Random(); 
-			int j = generator.nextInt(199) + 1;
+			int j = generator.nextInt(99) + 1;
 			arr[i]=j;
 		}
 		return arr;
@@ -29,11 +29,11 @@ public class heap_sort {
 		else{
 			sort(index*2+1, n);
 			sort(index*2+2, n);
-			trickle_down(index);
+			trickle_down(index, n);
 		}
 		
 	}
-	public static void trickle_down(int x){
+	public static void trickle_down(int x, int n){
 		System.out.println("next: "+x);
 		while (arr[x] < arr[2*x+1] ||
 				arr[x] < arr[2*x+2]){
@@ -45,6 +45,9 @@ public class heap_sort {
 			 else{
 				 x = 2*x+2; 
 			 }
+			 System.out.println("next: "+x);
+			 if (x>=n/2)
+				 break;
 		}
 	}
 	public static int max_child(int x){
