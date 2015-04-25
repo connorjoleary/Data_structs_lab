97ff39a1228bd92250e7d2fb006ff5a9eec36578
fippy24 <connor.trumpet@gmail.com>
2015-04-24 21:12:30 -0600

    make  initial tree

--- src/lab_ec/heap_sort.java
+++ src/lab_ec/heap_sort.java
@@ -1,10 +1,23 @@
 package lab_ec;
 
+import java.util.Random;
+
 public class heap_sort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Tree t = make_initial_tree();
+		t.displayTree();
+		
+	}
+	public static Tree make_initial_tree(){
+		Tree theTree = new Tree();
+		for (int i=0; i<5; i++){
+			Random generator = new Random(); 
+			int j = generator.nextInt(24) + 1;
+			theTree.insert(j);
+		}
+		return theTree;
 	}
 
 }
