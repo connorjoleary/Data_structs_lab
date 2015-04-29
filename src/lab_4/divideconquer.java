8f5afdd317c456e4129128d3431a45e56de60d5f
deadmau6 <joepop16@gmail.com>
2015-04-29 11:00:32 -0700

    start

--- src/lab_4/divideconquer.java
+++ src/lab_4/divideconquer.java
@@ -1,15 +1,22 @@
 package lab_4;
 
+import java.util.Random;
+
 /*Implement the divide-and-conquer algorithm you learnt in the class to solve the closest pair of
 points problem. You could either hard-code or randomly generate points in your solution. The
 running time of your algorithm should be O(nlogn). Sample output of your solution should
 look like:*/
 
 public class divideconquer {
+    public static double Input[][];
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+            //input
+            int ran = new Random();
+            for(int i = 0; i< ;i++){
+                
+            }
 	}
 
 }
