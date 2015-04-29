510d4638c9b8ad9e4bb8c74d62b82b88a3d6e9da
deadmau6 <joepop16@gmail.com>
2015-04-29 15:53:06 -0700

    sort y and hard code input

--- src/lab_4/divideconquer.java
+++ src/lab_4/divideconquer.java
@@ -8,11 +8,13 @@ running time of your algorithm should be O(nlogn). Sample output of your solutio
 look like:*/
 
 public class divideconquer {
-    public static double input[][];
+    //temp hard code
+    public static double [][] input = new double[][]{{2.0,4.0,5.0,10.0,14.0,15.0,17.0,19.0,22.0,25.0,29.0,30.0}, {7.0,13.0,8.0,5.0,9.0,5.0,7.0,10.0,7.0,10.0,14.0,2.0}};
+    //public static double input[][];
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-            input=new double[12][2];
+            /*input=new double[12][2];
             Random ran = new Random();
             for(int i = 0; i< 12;i++){
                 for(int j = 0; j< 2;j++){
@@ -20,7 +22,8 @@ public class divideconquer {
                     System.out.print(input[i][j]+ " ");
                 }
                 System.out.println("");
-            }
+            }*/
+            //temp hard code points
             sort_for_x();
             System.out.println("");
             for(int i = 0; i< 12;i++){
@@ -39,22 +42,40 @@ public class divideconquer {
 			input[j+1]=temp;
 		}
 	}
-	public static double closest_pair(int num,int p1_index, int p2_index){
-		if (num==1)
+        public static void sort_for_y(){
+		for (int i=1; i<input.length; i++){
+			double[] temp = input[i];
+			int j;
+			for (j=i-1; j>=0 && temp[0]<input[j][1];j--){
+				input[j+1]=input[j];
+			}
+			input[j+1]=temp;
+		}
+	}
+	public static double closest_pair(int s,int p1_index, int p2_index){
+		if (s==1){
 			return Double.POSITIVE_INFINITY;
-		if (num==2)
+                }
+		if (s==2){
 			return dist(input[p1_index],input[p2_index]);
-		double m = middle(input[p1_index][0],input[p2_index][0]);
+                }
+		int m = (p1_index+p2_index)/2;
 		//divide s into s1 and s2
 		
-		double d1=closest_pair(s1, )
+		double d1=closest_pair((m-p1_index)+1, p1_index, m );
+                double d2=closest_pair((m-p2_index)+1, m+1, p2_index);
+                double d3=closest_pair_xcut(m, s, d)
+                return(min(d1,d2,d3));
 	}
 	public static double dist(double[] p1, double[] p2){
 		return Math.sqrt(Math.pow((p1[0]+p2[0]),2)+Math.pow((p1[1]+p2[1]),2));
 	}
-	public static double middle(double x1, double x2){
-		return (x2-x1)/2;
-	}
+        public static double min(double d1, double d2 ,double d3){
+            return 0;
+        }
+        public static double closest_pair_xcut(m, s, d){
+            
+        }
 	
 }
 
