5b14081eca77ef67dc77692971e85a4499cab961
fippy24 <connor.trumpet@gmail.com>
2015-05-01 21:15:43 -0600

    fixing some stuff

--- src/lab_4/divideconquer.java
+++ src/lab_4/divideconquer.java
@@ -43,19 +43,21 @@ public class divideconquer {
 			input[j+1]=temp;
 		}
 	}
-        public static void sort_for_y(){
-		for (int i=1; i<input.length; i++){
-			double[] temp = input[i];
+        public static double[][] sort_for_y(double[][] arr){
+		for (int i=1; i<arr.length; i++){
+			double[] temp = arr[i];
 			int j;
-			for (j=i-1; j>=0 && temp[0]<input[j][1];j--){
-				input[j+1]=input[j];
+			for (j=i-1; j>=0 && temp[1]<arr[j][1];j--){
+				arr[j+1]=arr[j];
 			}
-			input[j+1]=temp;
+			arr[j+1]=temp;
 		}
+		return arr;
 	}
 	public static double closest_pair(int s,int p1_index, int p2_index){
             System.out.println("Solving Problem: Point["+p1_index+"] ... Point["+p2_index+"]");
-		if (s==1){
+            System.out.println(s);
+		if (s<=1){
 			return Double.POSITIVE_INFINITY;
                 }
 		if (s==2){
@@ -67,35 +69,37 @@ public class divideconquer {
 		double d1=closest_pair((m-p1_index)+1, p1_index, m );
                 System.out.println("Found Result At Points:("+input[p1_index][0]+","+input[p1_index][1]+")("+input[p2_index][0]+","+input[p2_index][1]+") Distance:"+d1); 
                 double d2=closest_pair((m-p2_index)+1, m+1, p2_index);
-                double d3=closest_pair_xcut(m, min(d1, d2, 0.0));
+                double[][] arr = Arrays.copyOfRange(input, p1_index, p2_index+1);
+                double d3=closest_pair_xcut(m, min(d1, d2, Double.POSITIVE_INFINITY), arr);
                 return(min(d1,d2,d3));
 	}
 	public static double dist(double[] p1, double[] p2){
 		return Math.sqrt(Math.pow((p1[0]-p2[0]),2)+Math.pow((p1[1]-p2[1]),2));
 	}
         public static double min(double d1, double d2 ,double d3){
-             if(d1 > d2 && d1 > d3){
+             if(d1 <= d2 && d1 <= d3){
                 return d1;
-            }else if(d2 > d1 && d2 > d3){
+            }else if(d2 <= d1 && d2 <= d3){
                 return d2;
-            }else if(d3 > d1 && d3 > d2){
+            }else if(d3 <= d1 && d3 <= d2){
                 return d3;
             }
             return 0;
         }
-        public static double closest_pair_xcut(int m, double d){
+        public static double closest_pair_xcut(int m, double d, double[][] s){
             System.out.println("Combining problems:");
-           // sort_for_y();
-            double [][] sc = {{0.0},{0.0}};
-            for(int i = 0; i<input.length;i++){
-                if((input[i][0]-m)<d){
-                    sc[i][0] = input[i][0];
-                    sc[i][1] = input[i][1];
+            double [][] s_y = sort_for_y(s);
+            ArrayList<double[]> s_c = new ArrayList<double[]>();
+            //System.out.println("Here: "+Arrays.deepToString(s));
+            //System.out.println("Here: "+Arrays.deepToString(s_y));
+            for(int i = 0; i<s_y.length;i++){
+                if(Math.abs(s_y[i][0]-m)<d){
+                    s_c.add(s_y[i]);
                 }
-            }for (int j = 0; j < input.length; j++) {
-                    if ((input[j][0] - sc[j][0]) < m) {
+            }for (int j = 0; j < s_c.size(); j++) {
+                    if ((s_c.get(j)[0] - s_y[j][0]) < m) {
                         for(int k =0; k<4;k++){
-                        d = min(d, dist(sc[j],sc[j+k]), dist(sc[j],sc[j-k]));
+                        d = min(d, dist(s_y[j],s_y[j+k]), dist(s_y[j],s_y[j-k]));
                         }     
                     }
                 }
